7be50a6bad5310827c0a1e4bf871d7c5680354db
luizfngui <luiz.guimaraes@vaivoa.com>
2022-03-02 23:11:53 +0000

    Bcript

--- Models/DTOs/UserView.cs
+++ Models/DTOs/UserView.cs
@@ -7,7 +7,17 @@ namespace MyWebAPI.Models.DTOs
 {
     public class UserView
     {
+        /// <example>
+        /// 00000000-0000-0000-0000-000000000000
+        /// </example>
         public Guid Id { get; set; }
+        /// <example>
+        /// ryan@reynolds.com
+        /// </example>
+        public string Email { get; set; }
+        /// <example>
+        /// Ryan Reynolds
+        /// </example>
         public string Name { get; set; }
         public DateTime CreatedAt { get; set; }
     }
